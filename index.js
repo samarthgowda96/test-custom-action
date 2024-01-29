262080dde989b848046aaed27f587cbfa819528a
samarthgowda96 <samarthgowda13@gmail.com>
2024-01-29 12:41:08 -0600

    auth details

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+import { AuthClient } from "@ihr-radioedit/service-auth";
 const core = require("@actions/core");
 const github = require("@actions/github");
 
 (
     async () => {
         try{
-           core.notice("calling our custom action")
+            const client = AuthClient.auto();
+            const me = await client.getMyPrincipal().ifLeft(e => console.error(e)).orDefault(null);
+            console.log(me);
+            core.notice("calling our custom action", me)
         }catch(e){
             core.setFailed(e.message)
         }
