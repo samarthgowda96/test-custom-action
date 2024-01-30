d4deec8805292a699c1568dc63cf8ab45b5013b4
samarthgowda96 <samarthgowda13@gmail.com>
2024-01-29 20:49:51 -0600

    auth details

--- index.mjs
+++ index.mjs
@@ -1,15 +1,28 @@
 
-import {AuthClient} from "@ihr-radioedit/service-auth";
-import { notice, setFailed } from "@actions/core";
-import github from "@actions/github";
 
+import { notice, setFailed, core } from "@actions/core";
+import { createRemoteJWKSet, jwtVerify } from "jose";
+
+
+export async function validateToken(
+        token
+    ) {
+        const jwks = createRemoteJWKSet(
+            new URL("https://token.actions.githubusercontent.com/.well-known/jwks"),
+        );
+        const { payload } = await jwtVerify(token, jwks, {
+            issuer: "https://token.actions.githubusercontent.com",
+        });
+    return payload;
+  }
+  
 (
     async () => {
         try{
-            const client = AuthClient.auto();
-            const me = await client.getMyPrincipal().ifLeft(e => console.error(e)).orDefault(null);
-            console.log(me);
-            notice("calling our custom action", me)
+            const token = await core.getIDToken();
+            const res = validateToken(token);
+            notice("calling our custom action", token)
+            notice("token verified woooo babyyy:", res)
         }catch(e){
             setFailed(e.message)
         }
